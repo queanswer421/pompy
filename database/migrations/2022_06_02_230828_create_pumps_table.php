4cffce0d5b8c2fb65267d50802f92aefd2116289
queanswer421 <queanswer421@gmail.com>
2022-06-07 02:05:40 +0200

    add migration

---
+++ database/migrations/2022_06_02_230828_create_pumps_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pumps', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('category');
+            $table->double('tempBiwa');
+            $table->double('power35');
+            $table->double('power45');
+            $table->double('power55');
+            $table->double('volume');
+            $table->integer('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pumps');
+    }
+};
