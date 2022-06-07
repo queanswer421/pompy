a3b5f33dfbbe40614eba2cbeb6d3f4aef99a6fcf
queanswer421 <queanswer421@gmail.com>
2022-06-07 02:28:03 +0200

    add houses

---
+++ app/Http/Controllers/HouseController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\House;
+use Illuminate\Http\Request;
+
+class HouseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $house = House::latest()->get();
+      
+        return view('house.index',compact('house'))
+                ->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('house.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        
+        $request->validate([
+            'surface' => 'required',
+            'type' => 'required',
+            'temp' => 'required',
+            'cwu' => 'required',
+        ]);
+        $request->heatDemand = 10;
+        // $request->surface*$request->type/1000;
+        House::create($request->all());
+       
+        return redirect()->route('houses.index')
+                        ->with('success','Wycena pompy dodana.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\House  $house
+     * @return \Illuminate\Http\Response
+     */
+    public function show(House $house)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\House  $house
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(House $house)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\House  $house
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, House $house)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\House  $house
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(House $house)
+    {
+        //
+    }
+}
---
+++ database/migrations/2022_06_03_004125_create_houses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('houses', function (Blueprint $table) {
+            $table->id();
+            $table->integer('surface');
+            $table->integer('type');
+            $table->double('heatDemand');
+            $table->double('temp');
+            $table->integer('cwu');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('houses');
+    }
+};
