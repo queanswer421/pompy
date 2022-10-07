a0f7e046700a6663c6df7c001de56a3e01de4666
Lukasz Hacas <queanswer421@gmail.com>
2022-10-07 14:05:01 +0200

    http

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ Route::get('/admin', function(){
 });
 //#############################################
 Route::get('/roger', function(){
+    return "testtetete";
     return Http::get('pompyapi.online:8000/admin/pumps');
 });
 
