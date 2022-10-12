344427bf94f271115f0aa5015b45e13228401bbf
Lukasz Hacas <queanswer421@gmail.com>
2022-10-12 02:30:15 +0200

    app js

--- resources/js/app/app.js
+++ resources/js/app/app.js
@@ -1,13 +1,14 @@
 require('./bootstrap');
 import Vue from 'vue'
-// import Vuetify from 'vuetify'
-// import 'vuetify/dist/vuetify.min.css'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 // import vuetify from './plugins/vuetify';
 import App from './App.vue'
 
-// Vue.use(Vuetify)
+Vue.use(Vuetify)
 
 const app = new Vue({
+    vuetify: new Vuetify,
     // vuetify,
     // vuetify: new Vuetify(),
     render: h => h(App),
