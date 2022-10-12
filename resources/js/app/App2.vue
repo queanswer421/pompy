51766725da15e44b1ad74ea7bd62bb44e310ccfb
Lukasz Hacas <queanswer421@gmail.com>
2022-10-12 02:19:20 +0200

    fill

---
+++ resources/js/app/App2.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-app>
+        <v-content>
+      <v-container class="fill-height" fluid>
+        APPPPP
+      </v-container>
+    </v-content>
+    </v-app>
+
+</template>
+
+<script>
+export default {
+  data: () => ({
+    step: 1
+  }),
+  props: {
+    source: String
+  }
+};
+</script>
--- resources/js/app/app.js
+++ resources/js/app/app.js
@@ -1,14 +1,14 @@
 require('./bootstrap');
 import Vue from 'vue'
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
+// import Vuetify from 'vuetify'
+// import 'vuetify/dist/vuetify.min.css'
 // import vuetify from './plugins/vuetify';
-import App from './App.vue'
+import App from './App2.vue'
 
-Vue.use(Vuetify)
+// Vue.use(Vuetify)
 
 const app = new Vue({
     // vuetify,
-    vuetify: new Vuetify(),
+    // vuetify: new Vuetify(),
     render: h => h(App),
 }).$mount("#app");
